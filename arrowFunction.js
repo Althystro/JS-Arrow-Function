3b0539f68b4ef3d8eaed4ee24d491b750dc6d2c4
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-10-07 15:01:30 +0300

    done and edited

--- arrowFunction.js
+++ arrowFunction.js
@@ -6,20 +6,20 @@
 ********************************/
 
 const greet = (name) => {
-  return console.log(`Hello, ${name}`);
+  return `Hello, ${name}`;
 };
-greet("wahab");
+console.log(greet("wahab"));
 // Write a simple arrow function that takes two parameters and returns their sum.
 
 const sumOfNumbers = (numberOne, numberTwo) => {
-  return console.log(numberOne + numberTwo);
+  return numberOne + numberTwo;
 };
-sumOfNumbers(3, 2);
+console.log(sumOfNumbers(3, 2));
 // Write a simple arrow function that squares a number.
 const squaredNumber = (number) => {
-  return console.log(number * number);
+  return number * number;
 };
-squaredNumber(9);
+console.log(squaredNumber(9));
 
 //challenge
 
